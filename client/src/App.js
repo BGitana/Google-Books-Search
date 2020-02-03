ac437da57be71faf570047ee012818e8fae8dbca
Gitana Brazlauskas <Bgitana@hotmail.com>
2020-02-03 17:10:31 -0500

    all functionalty is working

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/NavBar";
@@ -32,33 +31,3 @@ class App extends Component {
 };
 
 export default App;
-
-
-
-
-
-
-
-
-
-// import React, { Component } from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <div className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h2>Welcome to React</h2>
-//         </div>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -11,15 +11,3 @@ ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.unregister();
 
 
-
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./index.css";
-// import App from "./App";
-// import registerServiceWorker from "./registerServiceWorker";
-
-// ReactDOM.render(<App />, document.getElementById("root"));
-// registerServiceWorker();
-
-
